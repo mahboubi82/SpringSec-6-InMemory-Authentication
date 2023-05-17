283f2a7007ead90003c067d9d0af525b79100d86
MAHBOUBI <mahboubi82@gmail.comgit config --global color.ui autogit config --global user.email mahboubi82@gmail.com>
2023-05-17 23:59:04 +0100

    COMMIT 2

--- SpringMVC-Thymeleaf-SecureAppHotel-main/src/main/java/ma/mahboubi/hopital/HotelApplication.java
+++ SpringMVC-Thymeleaf-SecureAppHotel-main/src/main/java/ma/mahboubi/hopital/HotelApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import java.util.Date;
 
@@ -26,6 +28,21 @@ public class HotelApplication implements CommandLineRunner {
         patientRepository.save(Patient.builder().name("NOUREDDINE").dateNaissance(new Date()).score(4800).malade(true).build());
         patientRepository.save(Patient.builder().name("ABDOU").dateNaissance(new Date()).score(1400).malade(false).build());
         patientRepository.save(Patient.builder().name("TATA").dateNaissance(new Date()).score(7700).malade(true).build());
+    }
+    //@Bean
+    CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager){
+        PasswordEncoder passwordEncoder=passwordEncoder();
+        return args -> {
+
+            if (jdbcUserDetailsManager.userExists("user11")==false)
+            jdbcUserDetailsManager.createUser(User.withUsername("user11").password(passwordEncoder.encode("1234")).roles("USER").build());
+            if (jdbcUserDetailsManager.userExists("user22")==false)
+            jdbcUserDetailsManager.createUser(User.withUsername("user22").password(passwordEncoder.encode("1234")).roles("USER").build());
+            if (jdbcUserDetailsManager.userExists("admin")==false)
+            jdbcUserDetailsManager.createUser(User.withUsername("admin").password(passwordEncoder.encode("1234")).roles("ADMIN","USER").build());
+
+        };
+
     }
     @Bean
     public PasswordEncoder passwordEncoder(){
